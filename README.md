b07e82794bba245653ac2e341a1696e821f92d18
catkinson <catkinson@bloomhealthco.com>
2016-02-25 12:23:41 -0600

    Adding configuration to support running tests in Chrome

--- README.md
+++ README.md
@@ -9,6 +9,21 @@ The Geb test classes are in the ```src/integration-test/groovy``` directory.
 
 To execute the tests, run
 
+```grails test-app -integration```
+
+or
+
 ```gradlew integrationTest```
 
+
 By default this project runs the tests in Firefox, so you'll need to have Firefox installed.
+
+### Run tests in Chrome
+
+To run the tests in Chrome, we need to tell Geb to use the 'chrome' environment block in GebConfig.groovy. Run
+
+```grails -Dgeb.env=chrome test-app -integration```
+
+or
+
+```gradlew -Dgeb.env=chrome integrationTest```
--- src/integration-test/groovy/GebConfig.groovy
+++ src/integration-test/groovy/GebConfig.groovy
@@ -1,3 +1,5 @@
+import io.github.bonigarcia.wdm.ChromeDriverManager
+import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 
 // Location where Geb saves the screenshots and HTML dumps at the end of each test
@@ -5,3 +7,16 @@ reportsDir = 'build/test-reports'
 
 // Run tests in Firefox by default
 driver = { new FirefoxDriver() }
+
+environments {
+
+    // run as "grails -Dgeb.env=chrome test-app -integration"
+    // or "gradlew -Dgeb.env=chrome integrationTest"
+    // ChromeDriver reference: https://sites.google.com/a/chromium.org/chromedriver/
+    chrome {
+        // Download and configure ChromeDriver using https://github.com/bonigarcia/webdrivermanager
+        ChromeDriverManager.getInstance().setup()
+
+        driver = { new ChromeDriver() }
+    }
+}
